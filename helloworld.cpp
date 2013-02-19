e1e47b0439b7d37bc1adaec184c7af60ece9436f
AlexHanson <alex.j.hanson.14@dartmouth.edu>
2013-02-19 16:59:12 -0500

    hello world test

---
+++ helloworld.cpp
@@ -0,0 +1,20 @@
+//Alex Hanson
+//Project 1 -- Hello World
+//Date: 1-15-13
+//Some help from Eclipse "Hello World" project
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
+
+
+/* Output copy-pasted below
+ *
+ * !!!Hello World!!!
+ *
+ */
